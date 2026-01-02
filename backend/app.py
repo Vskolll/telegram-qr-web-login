03759e8a617b2bc1251d2b2ab0a3443c51cec696
Vskolll <lacunas.basest-0i@icloud.com>
2026-01-02 20:03:39 +0200

    11111

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
+
 from backend.logging_config import setup_logging
-    
 from backend.api.auth import router as auth_router
 from backend.api.ws import router as ws_router
 
@@ -13,7 +13,8 @@ setup_logging()
 app.include_router(auth_router, prefix="/auth")
 app.include_router(ws_router, prefix="/auth")
 
-app.mount("/static", StaticFiles(directory="backend/frontend"), name="static")
+# static
+app.mount("/static", StaticFiles(directory="backend/frontend/static"), name="static")
 
 @app.get("/")
 def index():
@@ -23,6 +24,14 @@ def index():
 def admin():
     return FileResponse("backend/frontend/admin.html")
 
-@app.get("/next")
+@app.get("/next", response_class=HTMLResponse)
 def next_page():
-    return FileResponse("backend/frontend/next.html")
+    with open("backend/frontend/next.html", "r", encoding="utf-8") as f:
+        html = f.read()
+
+    # ⬇️ сервер управляет таймером
+    return """
+    <script>
+      window.MATCH_TIMER_ENABLED = true;
+    </script>
+    """ + html
